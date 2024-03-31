2b52e855abeea80a01009a294c8b381e5f3fe3f2
Namyoungseo <nys8027@naver.com>
2024-03-31 23:16:34 +0900

    1차수정

--- config/blog/urls.py
+++ config/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/create', views.create, name ='create'),
+    path('blog/create/', views.create, name ='create'),
     path('blog/', views.detail, name='detail'),
-    ]
+]
